f19b964d5aca219da2549514e2b0603e14ca6af5
Filimon Mikhail <misafilimon255@mail.ru>
2020-07-11 16:02:11 +0300

    feature(js/script.js)complte the complicated lesson2

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* eslint-disable strict */
+'use strict';
+let num = 266219;
+num = num.toString().split('').reduce((sum, current) => +sum * (+current), 1) ** 3;
+num = +num.toString().slice(0, 2);
+document.querySelector('body').append(num);
